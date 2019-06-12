3be2f917c84e543c00b1e1b2890c99aaeebac460
zzlhye0307 <35554324+zzlhye0307@users.noreply.github.com>
2019-06-13 01:25:40 +0900

    Update compareDay.sql

--- DBproject/WebContent/sql/compareDay.sql
+++ DBproject/WebContent/sql/compareDay.sql
@@ -3,28 +3,32 @@ create or replace function compareDay(
   insertDay IN NUMBER)
   RETURN number
   IS
+  teach_firstNum NUMBER;
+  teach_secondNum NUMBER;
+  insert_firstNum NUMBER;
+  insert_secondNum NUMBER;
   conflict_checker NUMBER;
 BEGIN
+  teach_firstNum := 6;
+  teach_secondNum := 7;
+  insert_firstNum := 8;
+  insert_secondNum := 9;
   conflict_checker := 0;
-  IF (teachDay = insertDay) THEN
-    conflict_checker := 1;
-  ELSIF ((teachDay = 0 and insertDay = 5) or (teachDay = 5 and insertDay = 0)) THEN
-    conflict_checker := 1;
-  ELSIF (teachDay = 1 and insertDay = 6) THEN
-    conflict_checker := 1;
-  ELSIF (teachDay = 2 and (insertDay = 5 or insertDay = 7)) THEN
-    conflict_checker := 1;
-  ELSIF (teachDay = 3 and insertDay = 6) THEN
-    conflict_checker := 1;
-  ELSIF (teachDay = 4 and insertDay = 7) THEN
-    conflict_checker := 1;
-  ELSIF (teachDay = 5 and (insertDay = 0 or insertDay = 2 or insertDay = 7)) THEN
-    conflict_checker := 1;
-  ELSIF (teachDay = 6 and (insertDay = 1 or insertDay = 3)) THEN
-    conflict_checker := 1;
-  ELSIF (teachDay = 7 and (insertDay = 2 or insertDay = 4 or insertDay = 5)) THEN
-    conflict_checker := 1;
+  
+  IF (teachDay < 10) THEN
+	teach_secondNum := teachDay;
+  ELSIF (teachDay >= 10) THEN
+	teach_firstNum := SUBSTR(TO_CHAR(teachDay), 1, 1);
+	teach_secondNum := SUBSTR(TO_CHAR(teachDay), 2, 1);
   END IF;
+  
+  insert_firstNum := SUBSTR(TO_CHAR(insertDay), 1, 1);
+  insert_secondNum := SUBSTR(TO_CHAR(insertDay), 2, 1);
+  
+  IF ((teach_firstNum = insert_firstNum) or (teach_firstNum = insert_secondNum) or (teach_secondNum = insert_firstNum) or (teach_secondNum = insert_secondNum)) THEN
+	conflict_checker := 1;
+  END IF;
+
   RETURN conflict_checker;
 END;
 /
